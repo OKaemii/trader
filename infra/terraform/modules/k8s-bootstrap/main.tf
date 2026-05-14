9595b2f2b064bde6bbbe50a7104fc6444172e5e3
Xia <panxiaqi@gmail.com>
2026-05-14 02:56:52 +0100

    feat(infra/terraform): add demo and live T212 key-id secrets

--- infra/terraform/modules/k8s-bootstrap/main.tf
+++ infra/terraform/modules/k8s-bootstrap/main.tf
@@ -4,6 +4,18 @@ variable "internal_secret" { sensitive = true }
 variable "redis_password" { sensitive = true }
 variable "mongodb_password" { sensitive = true }
 variable "t212_api_key" { sensitive = true }
+variable "t212_api_key_id" {
+  sensitive = true
+  default   = ""
+}
+variable "t212_api_key_demo" {
+  sensitive = true
+  default   = ""
+}
+variable "t212_api_key_id_demo" {
+  sensitive = true
+  default   = ""
+}
 variable "resend_api_key" { sensitive = true }
 variable "notify_email" {}
 variable "seed_admin_email" { default = "" }
@@ -26,8 +38,11 @@ resource "kubernetes_secret" "trader_secrets" {
     INTERNAL_SECRET     = var.internal_secret
     REDIS_PASSWORD      = var.redis_password
     MONGODB_PASSWORD    = var.mongodb_password
-    T212_API_KEY        = var.t212_api_key
-    RESEND_API_KEY      = var.resend_api_key
+    T212_API_KEY         = var.t212_api_key
+    T212_API_KEY_ID      = var.t212_api_key_id
+    T212_API_KEY_DEMO    = var.t212_api_key_demo
+    T212_API_KEY_ID_DEMO = var.t212_api_key_id_demo
+    RESEND_API_KEY       = var.resend_api_key
     NOTIFY_EMAIL        = var.notify_email
     SEED_ADMIN_EMAIL    = var.seed_admin_email
     SEED_ADMIN_PASSWORD = var.seed_admin_password
